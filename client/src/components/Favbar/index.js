b0e0159902d2e12e0a37e4de5257f0df64b9e0b3
StevenVC-P <77998885+StevenVC-P@users.noreply.github.com>
2021-05-19 11:45:06 -0500

    changed product feed to come from database seed file instead of json file

--- client/src/components/Favbar/index.js
+++ client/src/components/Favbar/index.js
@@ -1,29 +1,29 @@
 import React, { useState, useEffect }  from "react";
 // import FavContext from "../../utils/FavContext";
 import FavSelector from "../FavSelector";
-import products from "../../products.json";
+// import products from "../../../../scripts/seedDB";
 
-function Favbar (props) {
-  const [favState, setFavState] = useState([])
+// function Favbar (props) {
+//   const [favState, setFavState] = useState([])
 
-  useEffect(() => {
-    setFavState(products)
-  }, [])
+//   useEffect(() => {
+//     setFavState(products)
+//   }, [])
 
-    return(
-      <div>
-        {favState.map(favPost => (
-          <FavSelector
-          key={favPost.id}
-          id={favPost.id}
-          name={favPost.name}
-          type={favPost.type}
-          review={favPost.review}
-          url={favPost.url}
-          />
-        ))}
-      </div>
-    )
-}
+//     return(
+//       <div>
+//         {favState.map(favPost => (
+//           <FavSelector
+//           key={favPost.id}
+//           id={favPost.id}
+//           name={favPost.name}
+//           type={favPost.type}
+//           review={favPost.review}
+//           url={favPost.url}
+//           />
+//         ))}
+//       </div>
+//     )
+// }
 
-export default Favbar;
+// export default Favbar;
--- client/src/pages/Submitpost/index.js
+++ client/src/pages/Submitpost/index.js
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from "react";
 import SubmitProduct from "../../components/SubmitProduct";
-import products from "../../products.json";
 import "./style.css";
 function Submitpost () {
     const [productState, setProductState] = useState([])
 
     useEffect(()=>{
-        setProductState(products)
+        setProductState(productState)
     }, []);
     return(
 
