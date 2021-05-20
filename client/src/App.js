79f4ee8a9e589c2e44dc9762b57f7fd88b9bcc1f
Adam Natrop <adamnatrop@gmail.com>
2021-05-20 14:27:54 -0500

    fixed routes with login

--- client/src/App.js
+++ client/src/App.js
@@ -27,32 +27,33 @@ function App() {
   return (
    <>
     { readyRender === true && (
-    <div>
-      <Router>
-      <Header setLoggedIn={setLoggedIn} loggedIn={loggedIn}/>
-      <Switch>
-      <Route exact path="/signup" setLoggedIn={setLoggedIn} component={SignUp}  />
-      <Route exact path="/home" component={Home} />
-        { loggedIn === false ? (
-          // <Landing setLoggedIn={setLoggedIn} />
-          <Route exact path="/" component={Landing} />
-        ) : (
-              <>
-              <Route exact path="/" component={Home} />
-                <Route exact path="/home" component={Home} />
-                <Route exact path="/swaps" component={Home} />
-                <Route exact path="/post" component={Post} />
-                <Route exact path="/product/:productId" component={Product}/>
-                <Route exact path="/submitpost" component={Submitpost} />
-              </>
-        )}
-        </Switch>
-      </Router>
-      <Footer />
-    </div>
+      <div>
+        <Router>
+        <Header setLoggedIn={setLoggedIn} loggedIn={loggedIn}/>
+        <Switch>
+        <Route exact path="/signup" setLoggedIn={setLoggedIn} component={SignUp}  />
+        <Route exact path="/home" component={Home} />
+          { loggedIn === false ? (
+            <Landing setLoggedIn={setLoggedIn} />
+          ) : (
+                <>
+                  <Route exact path="/swap" component={Home} />
+                  <Route exact path="/" component={Home} />
+                  <Route exact path="/post" component={Post} />
+                  <Route exact path="/product/:productId" component={Product}/>
+                  <Route exact path="/submitpost" component={Submitpost} />
+                </>
+          )}
+          </Switch>
+        </Router>
+        <Footer />
+      </div>
     )}
     </>
   );
 };
 
 export default App;
+
+
+
